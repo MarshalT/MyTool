880f04f67876f6f616cebd7de80304f9ef9f1cc4
溶进咖啡的糖 <602032825@qq.com>
2018-04-22 22:53:50 +0800

    Signed-off-by: 溶进咖啡的糖 <602032825@qq.com>

---
+++ msdn5box/Js示例.js
@@ -0,0 +1,17 @@
+/*数学运算*/
+function SumNum(x, y) {
+    return eval(x) + eval(y);
+}
+/*字符串运算*/
+function SumString(x, y) {
+    return x + y;
+}
+/*Eavl 与Run 执行本方法后请观察结果*/
+function Add(Num) {
+    return 1 + Num;
+}
+/*方法中使用字符串*/
+function Hellow(x) {
+    return 'Hellow' + x;
+   套近乎
+}
